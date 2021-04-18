71df5e15428c408b3346cc681e681de5ea68b93d
Gabi B <Gab3009@Gmail.com>
2021-04-19 02:21:20 +0300

    got somewhere

--- main.ts
+++ main.ts
@@ -9,18 +9,54 @@ const DT = 0.2
 const DIFF = 0.2
 const VISC = 0.1
 const SIGMOID_STEP = 12
+const FADE_FACTOR = 0.001
+const HALF_PI = Math.PI / 2
+const PARTICLES_COUNT = 2000
+
+const STEPS = [
+    [0, 1],
+    [0, -1],
+    [-1, 0],
+    [1, 0],
+    [1, 1],
+    [1, -1],
+    [-1, -1],
+    [-1, 1],
+]
 
 canvas.width = N * SCALE
 canvas.height = N * SCALE
 
 /* ---- utils ---- */
 const mySigmoid = (z: number, k: number): number => (2 / (1 + Math.exp(-z / k))) - 1
+const reverseSigmoid = (z: number, k: number, limit: number): number => ((limit * 2) / (1 + Math.exp(z / k)))
 
 const lerp = (a: number, b: number, k: number): number => a + k * (b - a)
 
 const inBounds = (x: number, y: number): boolean => (Math.floor(x / SCALE) < N && Math.floor(y / SCALE) < N)
 
-const keepInBounds = (num: number) => Math.min(Math.max(num, 0.5), N + 0.5)
+const keepInRange = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max)
+const keepInBounds = (num: number) => keepInRange(num, 0.5, N - 0.5)
+const keepInBox = (num: number) => keepInRange(num, 4, N * SCALE - 4)
+
+const coolColor = (x, y) => {
+    const nx = keepInRange(x, 0, 255)
+    const ny = keepInRange(y, 0, 255)
+    return `rgb(${0 + Math.floor(nx)}, ${0 + Math.floor(ny)}, 120)`
+}
+
+const coordinatesToDegree = (x: number, y: number): number => {
+    if (!x && !y) return 0
+    if (!y && x > 0) return 0
+    if (!y && x < 0) return Math.PI
+    if (!x && y > 0) return Math.PI / 2
+    if (!x && y < 0) return (3 * Math.PI) / 2
+
+    const degree = (Math.atan(y / x) + (2 * Math.PI)) % (2 * Math.PI)
+    if (y > 0 && x < 0) return degree - Math.PI
+    if (y < 0 && x < 0) return degree + Math.PI
+    return degree
+}
 
 const handleBounds = (grid: Cube[][], source: string, b: number) => {
     for (let i = 1; i < N; i++) {
@@ -127,6 +163,39 @@ const velStep = (grid: Cube[][]): void => {
     project(grid, 'vx', 'vy', 'prevVx', 'prevVy')
 }
 
+const customVelStep = (grid: Cube[][]): void => {
+    for (let i = 1; i < N; i++) {
+        for (let j = 1; j < N; j++) {
+            const v = Math.sqrt(grid[i][j].prevVx ** 2 + grid[i][j].prevVy ** 2)
+            if (v <= 0.7) continue
+            const velocityDegree = coordinatesToDegree(grid[i][j].prevVx, grid[i][j].prevVy)
+            STEPS.forEach(([x, y]) => {
+                const stepDegree = coordinatesToDegree(x, y)
+                let distBetween = Math.abs(stepDegree - velocityDegree)
+                let signedDistBetween = stepDegree - velocityDegree
+
+                if (distBetween >= HALF_PI) {
+                    distBetween = Math.abs(stepDegree + (2 * Math.PI) - velocityDegree)
+                    signedDistBetween = stepDegree + (2 * Math.PI) - velocityDegree
+                }
+                if (distBetween >= HALF_PI) {
+                    distBetween = Math.abs(stepDegree - (2 * Math.PI) - velocityDegree)
+                    signedDistBetween = stepDegree - (2 * Math.PI) - velocityDegree
+                }
+                if (distBetween >= HALF_PI) return
+
+                const newV = v * ((HALF_PI - distBetween) / HALF_PI)
+                const degreeDifference = Math.acos(newV / v)
+                let newD = velocityDegree
+                if (signedDistBetween >= 0) newD += degreeDifference
+                if (signedDistBetween < 0) newD -= degreeDifference
+                grid[x + i][y + j].vx += Math.cos(newD) * newV * 0.7
+                grid[x + i][y + j].vy += Math.sin(newD) * newV * 0.7
+            })
+        }
+    }
+}
+
 /* ---- classes ---- */
 
 // cude
@@ -157,18 +226,72 @@ class Cube {
         c.fillStyle = `rgba(255, 255, 255, ${mySigmoid(this.dens, SIGMOID_STEP)})`
         c.rect(this.x, this.y, SCALE, SCALE)
         c.fill()
-        // c.strokeStyle = 'black'
-        // c.stroke()
+    }
+    drawVector(): void {
+        c.beginPath()
+        c.moveTo(this.x + (SCALE / 2), this.y + (SCALE / 2))
+        c.lineTo(this.x + (SCALE / 2) + (this.vx * 10), this.y + (SCALE / 2) + (this.vy * 10))
+        c.strokeStyle = 'white'
+        c.stroke()
     }
     clonePrevs(): void {
         this.prevDens = this.dens
         this.prevVx = this.vx
         this.prevVy = this.vy
     }
+    clonePrevsAndClean(): void {
+        this.prevDens = this.dens
+        this.prevVx = this.vx
+        this.prevVy = this.vy
+        this.vx = 0
+        this.vy = 0
+    }
+    fade(): void {
+        this.dens *= (1 - FADE_FACTOR)
+        this.vx *= (1 - FADE_FACTOR)
+        this.vy *= (1 - FADE_FACTOR)
+    }
+}
+
+class Particle {
+    x: number
+    y: number
+    radius: number
+    vx: number
+    vy: number
+    constructor(x: number, y: number, radius: number = 2) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.vx = 0
+        this.vy = 0
+    }
+    update(grid: Cube[][]): void {
+        if (!inBounds(this.x, this.y) || (this.x < 0) || (this.y < 0)) {
+            this.x = keepInBox(this.x)
+            this.y = keepInBox(this.y)
+        }
+        const indexX = Math.floor(this.x / SCALE)
+        const indexY = Math.floor(this.y / SCALE)
+        this.vx += (grid[indexX][indexY].vx)
+        this.vy += (grid[indexX][indexY].vy)
+        this.x += this.vx
+        this.y += this.vy
+        this.vx *= 0.9
+        this.vy *= 0.9
+    }
+    draw(): void {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = coolColor(this.vx * 100, this.vy * 100)
+        c.fill()
+        c.fillStyle = 'white'
+    }
 }
 
 class Scene {
     grid: Cube[][]
+    particles: Particle[]
     constructor() {
         this.grid = []
         for (let i = 0; i <= N; i++) {
@@ -177,6 +300,13 @@ class Scene {
                 layer.push(new Cube(i * SCALE, j * SCALE))
             }
             this.grid.push(layer)
+        }
+        this.particles = []
+
+        for (let i = 0; i <= PARTICLES_COUNT; i++) {
+            const x = Math.floor(Math.random() * (N - 1) * SCALE) + 3
+            const y = Math.floor(Math.random() * (N - 1) * SCALE) + 3
+            this.particles.push(new Particle(x, y))
         }
     }
     draw(): void {
@@ -190,8 +320,12 @@ class Scene {
         velStep(this.grid)
         densStep(this.grid)
         this.draw()
+        this.grid.forEach(layer => layer.forEach(p => p.fade()))
+        this.particles.forEach(p => {
+            p.update(this.grid)
+            p.draw()
+        })
     }
-
 }
 
 /* ---- class instances ---- */
@@ -213,8 +347,8 @@ animate()
 window.addEventListener('mousemove', ({ x, y }) => {
     const { x: prevX, y: prevY } = mouseXY
     if (inBounds(x, y) && inBounds(prevX, prevY)) {
-        // const previndexX = Math.floor(prevX / SCALE)
-        // const previndexY = Math.floor(prevY / SCALE)
+        const previndexX = Math.floor(prevX / SCALE)
+        const previndexY = Math.floor(prevY / SCALE)
         const indexX = Math.floor(x / SCALE)
         const indexY = Math.floor(y / SCALE)
         const vx = mySigmoid(x - prevX, SIGMOID_STEP) * 50
@@ -223,6 +357,9 @@ window.addEventListener('mousemove', ({ x, y }) => {
         scene.grid[indexX][indexY].addSource('dens', Math.sqrt(vx ** 2 + vy ** 2))
         scene.grid[indexX][indexY].addSource('vx', vx)
         scene.grid[indexX][indexY].addSource('vy', vy)
+        scene.grid[previndexX][previndexY].addSource('dens', Math.sqrt(vx ** 2 + vy ** 2))
+        scene.grid[previndexX][previndexY].addSource('vx', vx)
+        scene.grid[previndexX][previndexY].addSource('vy', vy)
     }
     mouseXY.x = x
     mouseXY.y = y
